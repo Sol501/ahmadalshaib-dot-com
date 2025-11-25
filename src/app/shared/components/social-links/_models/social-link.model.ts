0f12e7de5bcb89f7177f70eaa3149676f1ffa007
Ahmad AlShaib <36101645+Sol501@users.noreply.github.com>
2025-11-25 09:31:41 +0300

    feat(@shared): Use icon component and update the type of social link type

--- src/app/shared/components/social-links/_models/social-link.model.ts
+++ src/app/shared/components/social-links/_models/social-link.model.ts
@@ -2,5 +2,10 @@ export interface SocialLink {
   readonly id: string;
   readonly label: string;
   readonly url: string;
-  readonly type: 'linkedin' | 'github' | string;
+  readonly type: SocialLinkTypeEnum;
+}
+
+export enum SocialLinkTypeEnum {
+  GitHub = 'github',
+  LinkedIn = 'linkedin'
 }
